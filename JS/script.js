9dfab6612d60b569bc55f576712e6e2dd2fc51a4
RNELSON <rinaldo.nelson1@gmail.com>
2020-04-05 12:15:18 +0800

    aafss

--- JS/script.js
+++ JS/script.js
@@ -8,9 +8,18 @@ function main()
     
     var customerID= ["theboss", "infinitystones", "diana"];
     
+
+    let IDsearch = document.querySelector("#IDS");
+
+    let withdrawal1 = document.querySelector("#WithdrawA");
+
     var IDsearch= document.getElementById("Search ID").customername.length;
     document.getElementById("IDS").innerHTML = IDsearch + "was found, please enter your deposit and withdrawal amount" 
 
+    var withdrawal1 = document.getElementById("withdrawal").balances.length;
+    document.getElementById("IDS").innerHTML = withdrawal1 + "was found, please enter your deposit and withdrawal amount" 
+
+    
 
     }
 main();
