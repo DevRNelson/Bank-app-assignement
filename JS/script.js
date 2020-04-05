5482dbe82f319fa6eba6c7bc2833760ba36c1918
RNELSON <rinaldo.nelson1@gmail.com>
2020-04-05 10:09:00 +0800

    var

--- JS/script.js
+++ JS/script.js
@@ -1,65 +1,19 @@
-class user
-{
 
-    //properties
-    fname;
-    lname;
-    balance;
-    customerid;
-
-    //Constructor
-    constructor(f,l,b,i)
-    {
-        this.fname=f;
-        this.lname=l;
-        this.balance=b;
-        this.customerid = i;
-    }
-    
-}
-
-
-//entry point of applicaiton 
 function main()
  {
 
-    //fectch elements from the DOM
-    const custID = document.querySelector("#customerID");
-    const withdraw= document.querySelector("#withdrawal");
-    const depo = document.querySelector("#deposits");
+    var customername= ["Bruce Wayne", "Thanos Badman", "Wonder Woman"];
 
-    const IDS = [];
-
-    index =0;
-
-    IDS.push(new custID("theboss"));
-    IDS.push(new custID("infinitystones"))
-    IDS.push(new custID( "Diana"))
-
-
+    var balances= ["4,000,000", "1000.00", "50,000.00"];
     
-    const withdraw = [];
-
-    index =0;
-
-    withd.push(new withdraw("4,000,000.00"));
-    withd.push(new withdraw("1000.00"))
-    withd.push(new withdraw("50,000.00"))
-
-
-    const depo= [];
-
-    index =0;
+    var customerID= ["theboss", "infinitystones", "diana"];
     
 }
 
     function fectch() {
-        deposits.fetch(value)
+        var x = document.getElementById("myForm").elements.length;
+        document.getElementById("demo").innerHTML = "Found " + x + " elements in the
+
+
     }
-
-    function myFunction() {
-        let addButton = document.querySelector("#findID");
-        addButton.addEventListener("click",findID);
-
-
 main();
